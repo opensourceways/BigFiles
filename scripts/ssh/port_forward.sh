d78e15d8905ef2f4b31675e6e7bb4b0bcdb14aea
Zherphy <1123678689@qq.com>
2024-12-06 17:12:59 +0800

    edit: edit DockerFile
    调试测试环境，进行ssh端口转发时需要使用1024以上的端口，这里选择2000端口

--- scripts/ssh/port_forward.sh
+++ scripts/ssh/port_forward.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshpass -p '123456' ssh -L 23231:gitee.com:22 -o ServerAliveInterval=20 -o StrictHostKeyChecking=no -gNf localhost
+sshpass -p '123456' ssh -L 23231:gitee.com:22 -o ServerAliveInterval=20 -o StrictHostKeyChecking=no -gNf -p 2000 localhost

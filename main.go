e40885f0079450caeaad613c9902a404159c648b
Wenl4ng <15861868579@163.com>
2025-01-03 16:26:44 +0800

    1

--- main.go
+++ main.go
@@ -58,6 +58,15 @@ func gatherOptions(fs *flag.FlagSet, args ...string) (options, error) {
 	return o, err
 }
 
+type User struct {
+	Id       uint   `gorm:"AUTO_INCREMENT"`
+	Name     string `gorm:"size:50"`
+	Age      int    `gorm:"size:3"`
+	Birthday *time.Time
+	Email    string `gorm:"type:varchar(50);unique_index"`
+	PassWord string `gorm:"type:varchar(25)"`
+}
+
 func main() {
 	o, err := gatherOptions(
 		flag.NewFlagSet(os.Args[0], flag.ExitOnError),
@@ -106,6 +115,41 @@ func main() {
 
 		return
 	}
+
+	db := db.DB()
+
+	// 自动迁移数据结构(table schema)
+	// 注意:在gorm中，默认的表名都是结构体名称的复数形式，比如User结构体默认创建的表为users
+	// db.SingularTable(true) 可以取消表名的复数形式，使得表名和结构体名称一致
+	db.AutoMigrate(&User{})
+
+	// 插入记录
+	db.Create(&User{Name: "wl", Age: 18, Email: "bgbiao@bgbiao.top"})
+	db.Create(&User{Name: "jfq", Age: 18, Email: "xxb@bgbiao.top"})
+
+	var user User
+	var users []User
+	// 查看插入后的全部元素
+	fmt.Printf("插入后元素:\n")
+	db.Find(&users)
+	fmt.Println(users)
+
+	// 查询一条记录
+	db.First(&user, "name = ?", "bgbiao")
+	fmt.Println("查看查询记录:", user)
+
+	// 更新记录(基于查出来的数据进行更新)
+	db.Model(&user).Update("name", "biaoge")
+	fmt.Println("更新后的记录:", user)
+
+	// 删除记录
+	db.Delete(&user)
+
+	// 查看全部记录
+	fmt.Println("查看全部记录:")
+	db.Find(&users)
+	fmt.Println(users)
+
 	s, err := server.New(server.Options{
 		Prefix:          cfg.Prefix,
 		Bucket:          cfg.LfsBucket,

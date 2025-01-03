7391520443230f117a09ca005e50f956c800433d
Wenl4ng <15861868579@163.com>
2025-01-03 16:05:43 +0800

    add

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/metalogical/BigFiles/utils"
 )
@@ -16,6 +18,7 @@ type Config struct {
 	ObsAccessKeyId     string         `json:"OBS_ACCESS_KEY_ID"`
 	ObsSecretAccessKey string         `json:"OBS_SECRET_ACCESS_KEY"`
 	ValidateConfig     ValidateConfig `json:"VALIDATE_REGEXP"`
+	DBConfig           DBConfig       `json:"DATABASE"`
 	DefaultToken       string         `json:"DEFAULT_TOKEN"`
 }
 
@@ -24,6 +27,17 @@ type ValidateConfig struct {
 	RepoNameRegexp string `json:"REPONAME_REGEXP"         required:"true"`
 	UsernameRegexp string `json:"USERNAME_REGEXP"         required:"true"`
 	PasswordRegexp string `json:"PASSWORD_REGEXP"         required:"true"`
+}
+
+type DBConfig struct {
+	DatabaseUserName string `json:"USERNAME"        required:"true"`
+	DatabaseName     string `json:"NAME"         required:"true"`
+	DatabasePassword string `json:"PASSWORD"         required:"true"`
+	DatabasePort     string `json:"PORT"         required:"true"`
+	DatabaseAddress  string `json:"ADDRESS"         required:"true"`
+	Life             int    `json:"LIFE"     required:"true"`
+	MaxIdle          int    `json:"MAXIDLE" required:"true"`
+	MaxConn          int    `json:"MAXCONN" required:"true"`
 }
 
 // LoadConfig loads the configuration file from the specified path and deletes the file if needed
@@ -36,4 +50,16 @@ func LoadConfig(path string, cfg *Config, remove bool) error {
 		return err
 	}
 	return nil
+}
+
+func (p *DBConfig) Dsn() string {
+
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		p.DatabaseUserName, p.DatabasePassword, p.DatabaseAddress, p.DatabasePort, p.DatabaseName)
+
+}
+
+func (cfg *DBConfig) GetLifeDuration() time.Duration {
+	return time.Minute * time.Duration(cfg.Life)
 }
